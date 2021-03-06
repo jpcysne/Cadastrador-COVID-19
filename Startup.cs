0f2505c350fc9e26900cd16855964dd8bd5496ee
jp <joaopaulorubio@hotmail.com>
2021-03-05 22:05:00 -0300

    comite

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using Cadastrador_COVID_19.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,10 @@ namespace Cadastrador_COVID_19
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddScoped<PacienteService>();
+            services.AddScoped<LocalVacinacaoService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
