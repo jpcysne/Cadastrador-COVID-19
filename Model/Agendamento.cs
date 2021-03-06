1ddc5be47330473f017ed8ea9f78ec9c53be78e9
jp <joaopaulorubio@hotmail.com>
2021-03-05 22:36:40 -0300

    testes

---
+++ Model/Agendamento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cadastrador_COVID_19.Model
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+
+        public Paciente Paciente { get; set; }
+        public LocalVacinacao LocalVacinacao { get; set; }
+
+        public Agendamento()
+        {
+        }
+
+        public Agendamento(int id, Paciente paciente, LocalVacinacao localVacinacao)
+        {
+            Id = id;
+            Paciente = paciente;
+            LocalVacinacao = localVacinacao;
+        }
+    }
+}
--- Model/LocalVacinacao.cs
+++ Model/LocalVacinacao.cs
@@ -11,6 +11,7 @@ namespace Cadastrador_COVID_19.Model
         public string Nome { get; set; }
         public string Endereco { get; set; }
         public int QuantidadesVagas { get; set; }
+        public int QuantidadesMaxVagas { get; set; }
         public DateTime dataVacinacao { get; set; }
 
         public ICollection<Paciente> Pacientes { get; set; } = new List<Paciente>();
@@ -18,6 +19,15 @@ namespace Cadastrador_COVID_19.Model
 
         public LocalVacinacao()
         {
+        }
+
+        public LocalVacinacao(int id, string nome, string endereco, int quantidadesVagas, DateTime dataVacinacao)
+        {
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+            QuantidadesVagas = quantidadesVagas;
+            this.dataVacinacao = dataVacinacao;
         }
 
         public LocalVacinacao(int id, string nome, string endereco, int quantidadesVagas, DateTime dataVacinacao, ICollection<Paciente> pacientes)
